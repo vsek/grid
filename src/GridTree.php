94b5693bf9a78c2a8e7195835e24f348cdd620b2
Václav Stodůlka <vsek@seznam.cz>
2015-12-02 11:19:58 +0100

    Přidáno zobrazení stromu, přidán sloupeček Ano/Ne

---
+++ src/GridTree.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Grid;
+
+/**
+ * Description of GridTree
+ *
+ * @author Vsek
+ */
+class GridTree extends Grid{
+    
+    protected $related;
+    
+    /**
+     * Nastavi pocet polozek na stranku
+     * @param int $itemsToPage
+     */
+    public function setItemsToPage($itemsToPage){
+        throw new \Exception($this->getPresenter()->translator->translate('admin.grid.paggindDisableForTree'));
+    }
+    
+    public function __construct($related, \Nette\ComponentModel\IContainer $parent = NULL, $name = NULL) {
+        parent::__construct($parent, $name);
+
+        $this->templateFile = 'gridTree.latte';
+        $this->templateDir = __DIR__;
+        $this->related = $related;
+    }
+    
+    public function setModel(\Nette\Database\Table\Selection $model) {
+        parent::setModel($model->where('parent_id ?', null));
+    }
+    
+    public function render(){
+        $template = $this->getTemplate();
+        $template->setFile($this->templateDir . '/' . $this->templateFile);
+        $template->setTranslator($this->getPresenter()->translator);
+        
+        //upravim model
+        if(!is_null($this->order)){
+            $this->model->order($this->order . ' ' . $this->orderDir);
+        }
+
+        $template->columns = $this->columns;
+        $template->model = $this->model;
+        $template->menu = $this->menu;
+        $template->uniquete = $this->uniquete;
+        $template->emptyText = is_null($this->emptyText) ? $this->getPresenter()->translator->translate('admin.grid.noItemFound') : $this->emptyText;
+        $template->visualPaginator = $this->visualPaginator;
+        $template->order = $this->order;
+        $template->orderDir = $this->orderDir;
+        $template->ordering = $this->ordering;
+        $template->related = $this->related;
+        
+        $template->render();
+    }
+}
---
+++ src/column/YesNo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Grid\Column;
+
+/**
+ * Description of YesNo
+ *
+ * @author Vsek
+ */
+class YesNo extends Column{
+    /**
+     * Vrati hodnotu z tabulky
+     * @param Nette\Database\Table\ActiveRow $query
+     * @return string
+     */
+    public function output(\Nette\Database\Table\ActiveRow $query){
+        $column = $this->column;
+        if($query->$column == 'yes'){
+            return 'Ano';
+        }else{
+            return 'Ne';
+        }
+    }
+}
